035b74cd9cbddc2e3059edaef01ed196bac40fb0
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2016-11-23 16:33:55 -0300

    AE

--- R01-01-Rot-SimpleSorting-SimultaneousRecursive-Bubble-environment/src/main/java/sorting/simpleSorting/SelectionSort.java
+++ R01-01-Rot-SimpleSorting-SimultaneousRecursive-Bubble-environment/src/main/java/sorting/simpleSorting/SelectionSort.java
@@ -4,7 +4,7 @@ import sorting.AbstractSorting;
 
 /**
  * The selection sort algorithm chooses the smallest element from the array and
- * puts it in the first position. Then chooses the second smallest element and
+ * puts  it in the first position. Then chooses the second smallest element and
  * stores it in the second position, and so on until the array is sorted.
  */
 public class SelectionSort<T extends Comparable<T>> extends AbstractSorting<T> {
