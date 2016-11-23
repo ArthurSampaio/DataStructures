2dfa90be45d8186139c44721d21134466592c6ff
ArthurSampaio <arthursampaio@lsd.ufcg.edu.br>
2016-11-23 16:31:38 -0300

    one more lab

--- RR2-01-Roteiro-Revisao-Generics-environment/src/main/java/solucao/vetor/TestarVetor.java
+++ RR2-01-Roteiro-Revisao-Generics-environment/src/main/java/solucao/vetor/TestarVetor.java
@@ -6,66 +6,60 @@ import static org.junit.Assert.*;
 
 import java.util.Comparator;
 
-import org.junit.Before;
-import org.junit.Test;
-
-
 public class TestarVetor {
 
-	public static void main(String[] args) {
-		Vetor<Aluno> vetor = new Vetor<Aluno>(20);
-		ComparadorMaximo compMax = new ComparadorMaximo();
-		vetor.setComparadorMaximo(compMax);
-		ComparadorMinimo compMin = new ComparadorMinimo();
-		vetor.setComparadorMinimo(compMin);
-		
-		Aluno arthur = new Aluno("Arthur", 7.8);
-		Aluno jose = new Aluno("Jose", 5.0);
-		Aluno bianca = new Aluno("Bianca", 6.5);
-		Aluno pedro = new Aluno("Pedro", 9.5);
-		
-		//verifica se o vetor está vazio
-		assertTrue(vetor.isVazio());
-		vetor.inserir(arthur);
-		vetor.inserir(jose);
-		vetor.inserir(bianca);
-		assertNull(vetor.procurar(pedro));
-		vetor.inserir(pedro);
-		
-		//procura
-		assertEquals(pedro, vetor.procurar(pedro));
-		//remove
-		vetor.remover(jose);
-		assertNull(vetor.remover(jose));
-		
-		assertEquals(pedro, vetor.maximo());
-		assertEquals(bianca, vetor.minimo());
-		
-		//adicionando jose de novo
-		vetor.inserir(jose);
-		//esta cheio?
-		assertFalse(vetor.isCheio());
-		System.out.println("Passou");
+   public static void main(String[] args) {
+      Vetor<Aluno> vetor = new Vetor<Aluno>(20);
+      ComparadorMaximo compMax = new ComparadorMaximo();
+      vetor.setComparadorMaximo(compMax);
+      ComparadorMinimo compMin = new ComparadorMinimo();
+      vetor.setComparadorMinimo(compMin);
 
-		
-	}
+      Aluno arthur = new Aluno("Arthur", 7.8);
+      Aluno jose = new Aluno("Jose", 5.0);
+      Aluno bianca = new Aluno("Bianca", 6.5);
+      Aluno pedro = new Aluno("Pedro", 9.5);
+
+      //verifica se o vetor está vazio
+      assertTrue(vetor.isVazio());
+      vetor.inserir(arthur);
+      vetor.inserir(jose);
+      vetor.inserir(bianca);
+      assertNull(vetor.procurar(pedro));
+      vetor.inserir(pedro);
+
+      //procura
+      assertEquals(pedro, vetor.procurar(pedro));
+      //remove
+      vetor.remover(jose);
+      assertNull(vetor.remover(jose));
+
+      assertEquals(pedro, vetor.maximo());
+      assertEquals(bianca, vetor.minimo());
+
+      //adicionando jose de novo
+      vetor.inserir(jose);
+      //esta cheio?
+      assertFalse(vetor.isCheio());
+      System.out.println("Passou");
+
+   }
 }
 
 class ComparadorMinimo implements Comparator<Aluno> {
 
-	@Override
-	public int compare(Aluno o1, Aluno o2) {
-		return (int) (o2.getMedia() - o1.getMedia());
-	}
+   @Override
+   public int compare(Aluno o1, Aluno o2) {
+      return (int) (o2.getMedia() - o1.getMedia());
+   }
 
 }
 
-
 class ComparadorMaximo implements Comparator<Aluno> {
 
-	@Override
-	public int compare(Aluno o1, Aluno o2) {
-		return (int) (o1.getMedia() - o2.getMedia());
-	}
+   @Override
+   public int compare(Aluno o1, Aluno o2) {
+      return (int) (o1.getMedia() - o2.getMedia());
+   }
 
 }
--- RR2-01-Roteiro-Revisao-Generics-environment/src/main/java/solucao/vetor/Vetor.java
+++ RR2-01-Roteiro-Revisao-Generics-environment/src/main/java/solucao/vetor/Vetor.java
@@ -2,8 +2,6 @@ package solucao.vetor;
 
 import java.util.Comparator;
 
-import vetor.Aluno;
-
 /**
  * Implementação de um vetor de objetos simples para exercitar os conceitos de
  * Generics.
@@ -13,110 +11,109 @@ import vetor.Aluno;
  */
 public class Vetor<T extends Comparable<T>> {
 
-	// O array interno onde os objetos manipulados são guardados
-	private T[] arrayInterno;
+   // O array interno onde os objetos manipulados são guardados
+   private T[] arrayInterno;
 
-	// O tamanho que o array interno terá
-	private int tamanho;
+   // O tamanho que o array interno terá
+   private int tamanho;
 
-	// Indice que guarda a proxima posição vazia do array interno
-	private int indice;
+   // Indice que guarda a proxima posição vazia do array interno
+   private int indice;
 
-	// O Comparators a serem utilizados
-	private Comparator<T> comparadorMaximo;
-	private Comparator<T> comparadorMinimo;
+   // O Comparators a serem utilizados
+   private Comparator<T> comparadorMaximo;
+   private Comparator<T> comparadorMinimo;
 
-	public Vetor(int tamanho) {
-		super();
-		this.tamanho = tamanho;
-		this.indice = -1;
-		this.arrayInterno = (T[]) new Comparable[tamanho];
-	}
+   public Vetor(int tamanho) {
+      super();
+      this.tamanho = tamanho;
+      this.indice = -1;
+      this.arrayInterno = (T[]) new Comparable[tamanho];
+   }
 
-	public void setComparadorMaximo(Comparator<T> comparadorMaximo) {
-		this.comparadorMaximo = comparadorMaximo;
-	}
+   public void setComparadorMaximo(Comparator<T> comparadorMaximo) {
+      this.comparadorMaximo = comparadorMaximo;
+   }
 
-	public void setComparadorMinimo(Comparator<T> comparadorMinimo) {
-		this.comparadorMinimo = comparadorMinimo;
-	}
+   public void setComparadorMinimo(Comparator<T> comparadorMinimo) {
+      this.comparadorMinimo = comparadorMinimo;
+   }
 
-	// Insere um objeto no vetor
-	public void inserir(T o) {
-		this.arrayInterno[++indice] = o;
-	}
+   // Insere um objeto no vetor
+   public void inserir(T o) {
+      this.arrayInterno[++indice] = o;
+   }
 
-	// Remove um objeto do vetor
-	public T remover(T o) {
-		// a procura se darah usando o equals o tipo T (precisa ser
-		// sobrescrito). a remocao
-		// nao deixa buracos no array
-		T result = null;
-		boolean achou = false;
-		int i = 0;
-		while (i <= indice && !achou) {
-			if (arrayInterno[i].equals(o)) {
-				result = arrayInterno[i];
-				arrayInterno[i] = arrayInterno[indice];
-				arrayInterno[indice] = null;
-				indice--;
-				achou = true;
-			}
-			i++;
-		}
-		return result;
-	}
+   // Remove um objeto do vetor
+   public T remover(T o) {
+      // a procura se darah usando o equals o tipo T (precisa ser
+      // sobrescrito). a remocao
+      // nao deixa buracos no array
+      T result = null;
+      boolean achou = false;
+      int i = 0;
+      while (i <= indice && !achou) {
+         if (arrayInterno[i].equals(o)) {
+            result = arrayInterno[i];
+            arrayInterno[i] = arrayInterno[indice];
+            arrayInterno[indice] = null;
+            indice--;
+            achou = true;
+         }
+         i++;
+      }
+      return result;
+   }
 
-	// Procura um elemento no vetor
-	public T procurar(T o) {
-		T result = null;
-		boolean achou = false;
-		int i = 0;
-		while (i <= indice && !achou) {
-			if (arrayInterno[i].equals(o)) {
-				result = arrayInterno[i];
-				achou = true;
-			}
-			i++;
-		}
-		return result;
-	}
+   // Procura um elemento no vetor
+   public T procurar(T o) {
+      T result = null;
+      boolean achou = false;
+      int i = 0;
+      while (i <= indice && !achou) {
+         if (arrayInterno[i].equals(o)) {
+            result = arrayInterno[i];
+            achou = true;
+         }
+         i++;
+      }
+      return result;
+   }
 
-	// Diz se o vetor está vazio
-	public boolean isVazio() {
-		return this.indice == -1;
-	}
+   // Diz se o vetor está vazio
+   public boolean isVazio() {
+      return this.indice == -1;
+   }
 
-	// Diz se o vetor está cheio
-	public boolean isCheio() {
-		return this.indice == this.tamanho - 1;
-	}
+   // Diz se o vetor está cheio
+   public boolean isCheio() {
+      return this.indice == this.tamanho - 1;
+   }
 
-	public T maximo() {
-		T result = null;
-		if (!isVazio()) {
-			result = arrayInterno[0];
-			for (int i = 0; i <= indice; i++) {
-				if (comparadorMaximo.compare(result, arrayInterno[i]) < 0) {
-					result = arrayInterno[i];
-				}
-			}
-		}
-		return result;
-	}
+   public T maximo() {
+      T result = null;
+      if (!isVazio()) {
+         result = arrayInterno[0];
+         for (int i = 0; i <= indice; i++) {
+            if (comparadorMaximo.compare(result, arrayInterno[i]) < 0) {
+               result = arrayInterno[i];
+            }
+         }
+      }
+      return result;
+   }
 
-	public T minimo() {
-		T result = null;
-		if (!isVazio()) {
-			result = arrayInterno[0];
-			for (int i = 0; i <= indice; i++) {
-				if (comparadorMinimo.compare(result, arrayInterno[i]) < 0) {
-					result = arrayInterno[i];
-				}
-			}
-		}
-		return result;
-	}
+   public T minimo() {
+      T result = null;
+      if (!isVazio()) {
+         result = arrayInterno[0];
+         for (int i = 0; i <= indice; i++) {
+            if (comparadorMinimo.compare(result, arrayInterno[i]) < 0) {
+               result = arrayInterno[i];
+            }
+         }
+      }
+      return result;
+   }
 
 }
-
